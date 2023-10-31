b7aa5a6e003021c9aa6c4a3a91c2cd5beaf39731
PC-Ngumoha <pcngumoha@gmail.com>
2023-10-31 21:42:06 +0100

    Update: Setup basic internationalization

---
+++ 0x02-i18n/3-app.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+"""
+1-app.py
+
+Basic setup for internationalization using Flask-Babel
+"""
+from flask import Flask, render_template, request
+from flask_babel import Babel
+
+
+class Config:
+    """
+    class Config
+
+    Provides configuration info for flask app instance
+    """
+    LANGUAGES = ['en', 'fr']
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
+app = Flask(__name__)
+app.config.from_object(Config())
+babel = Babel(app)
+
+
+@babel.localeselector
+def get_locale() -> str:
+    """
+    get_locale() babel handler function
+    """
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+@app.route('/', strict_slashes=False)
+def get_template_page() -> str:
+    """
+    get_template_page() route handler
+    """
+    return render_template('3-index.html')
+
+
+if __name__ == '__main__':
+    app.run(port=5001)
