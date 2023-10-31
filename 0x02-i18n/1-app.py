1599049147bf30336349f3a7a5c7562c1f00f392
PC-Ngumoha <pcngumoha@gmail.com>
2023-10-31 20:58:23 +0100

    Update: Create 1-app.py file

---
+++ 0x02-i18n/1-app.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+"""
+1-app.py
+
+Basic setup for internationalization using Flask-Babel
+"""
+from flask import Flask, render_template
+from flask_babel import Babel
+
+
+class Config:
+    """
+    class Config
+
+    Provides configuration info for flask app instance
+    """
+    LANGUAGES = ['en', 'fr']
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_TIMEZONE = 'UTC'
+
+
+app = Flask(__name__)
+app.config.from_object(Config())
+babel = Babel(app)
+
+
+@app.route('/', strict_slashes=False)
+def get_template_page() -> str:
+    """
+    get_template_page() route handler
+    """
+    return render_template('1-index.html')
+
+
+if __name__ == '__main__':
+    app.run(port=5001)
